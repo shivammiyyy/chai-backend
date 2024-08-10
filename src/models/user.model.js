6150a605705b38470a0d5e0d2b712874d89b758b
Shivam Kumar <shivamprofessional1@gmail.com>
2024-08-10 14:03:51 +0530

    ADD MODELS

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,8 +59,8 @@ userSchema.method.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken = fuction(){
-    jwt.sign(
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
         {
             _id : this._id,
             email : this.email,
@@ -74,7 +74,7 @@ userSchema.methods.generateAccessToken = fuction(){
     )
 }
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id : this._id
         },
